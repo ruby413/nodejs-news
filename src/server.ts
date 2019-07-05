4486682eb0f472c2a4eb4604b3f8d99a5a5dcab3
MCC <cocahack@naver.com>
2019-07-05 12:36:23 +0900

    feat: 프로젝트 구조 완성

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import server from "./app";
+const port = 3000;
+
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
