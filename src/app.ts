c2e6c18ca683cf97d7ffa9d4f0f8c77542f8eb84
MCC <cocahack@naver.com>
2019-07-05 23:36:18 +0900

    refactor: app.ts 미들웨어 추가
    앱에 필요한 미들웨어 추가 설정
    mongoose, passport 등 설정은 나중에 처리

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import createError from "http-errors";
+import logger from "morgan";
+import path from "path";
+
+import homeController from "./controllers/home";
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../src/public")));
+
+app.use("/", homeController);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error", {statusCode: err.status, errorMessage: err.message});
 });
 
 export default app;
