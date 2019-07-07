5a3ce51f084cd3757df9ccee5d6b8d9936a9cbce
MCC <cocahack@naver.com>
2019-07-07 17:40:31 +0900

    refactor: app.js 수정 후 컴파일

--- dist/app.js
+++ dist/app.js
@@ -2,11 +2,37 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importDefault(require("http-errors"));
+const morgan_1 = __importDefault(require("morgan"));
+const path_1 = __importDefault(require("path"));
+const homeController = __importStar(require("./controllers/home"));
 const app = express_1.default();
-app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
+app.set("views", path_1.default.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(morgan_1.default("dev"));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cookie_parser_1.default());
+app.use(express_1.default.static(path_1.default.join(__dirname, "../src/public")));
+app.use("/", homeController.index);
+app.use((req, res, next) => {
+    next(http_errors_1.default(404));
+});
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error", { statusCode: err.status, errorMessage: err.message });
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
