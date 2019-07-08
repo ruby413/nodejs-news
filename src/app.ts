a77c1895c4bf387e6a48ee0507d334906ddb4f8c
MCC <cocahack@naver.com>
2019-07-08 19:20:20 +0900

    feat: app.ts DB 관련 구성 추가
    - mongodb 연결
    - 라우터 연결

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@ import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
 import path from "path";
+import connect from "./connect";
 
-import homeController from "./controllers/home";
+import homeRouter from "./routes/home";
+import userRouter from "./routes/user";
+
+const db = "mongodb://mongoadmin:codesquad@localhost:22222/newstest?authSource=admin";
+connect({db});
 
 const app = express();
 
@@ -17,7 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../src/public")));
 
-app.use("/", homeController);
+app.use("/", homeRouter);
+app.use("/user", userRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
@@ -28,7 +34,12 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error", {statusCode: err.status, errorMessage: err.message});
+  res.render("error", {
+    env: req.app.get("env"),
+    errorMessage: err.message,
+    stackTrace: err.stack,
+    statusCode: err.status,
+  });
 });
 
 export default app;
